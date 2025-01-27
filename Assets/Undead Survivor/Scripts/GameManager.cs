eab0f4065c257ba5d89f88a298cc826cc9cb1943
zang-gang7 <jeeyang01@gmail.com>
2025-01-28 08:36:46 +0900

    뱀서라이크 10
    HUD 제작하기
    
    에러: Health 바가 플레이어 위치에 잘 따라 다녀야 되는데 이리저리 날아다님

--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     public float gameTime;
     public float maxGameTime = 2 * 10f;
     [Header("# Player Info")]
+    public int health;
+    public int maxHealth = 100;
     public int level;
     public int kill;
     public int exp;
@@ -20,6 +22,11 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         instance = this;
+    }
+
+    void Start()
+    {
+        health = maxHealth;
     }
 
     void Update()
