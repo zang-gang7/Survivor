e90721189ad0fac27882a95b8ff40fb2a8b8192b
SUNY-YoungChanKim <62223904+SUNY-YoungChanKim@users.noreply.github.com>
2025-02-12 19:05:43 +0900

    토끼 이속빨라지는거 수정

--- Assets/Undead Survivor/Scripts/BunnyMoveSpeed.cs
+++ Assets/Undead Survivor/Scripts/BunnyMoveSpeed.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class BunnyMoveSpeed : MonoBehaviour
 {
+    public float maxSpeed = 10f;
     public float speed = 1.5f;
     public float speedIncrease = 1f;
     public float increaseInterval = 3f;
@@ -15,14 +16,19 @@ public class BunnyMoveSpeed : MonoBehaviour
         InvokeRepeating("SpeedIncrease", increaseInterval, increaseInterval);
     }
 
+    //
+    /*Enemy에 이미 속도가 있고, 속도에 맞춰 위치 자동으로 이동 중이라 필요 없음
     private void FixedUpdate()
     {
         transform.position = Vector3.zero * speed * Time.deltaTime;
     }
+    */
 
 
+    //속도가 너무 빨라지면 텔레포트 할거라 맥스 속도 지정해야함
     private void SpeedIncrease()
     {
-        speed += speedIncrease;
+        if (GetComponent<Enemy>() is Enemy enemyComponenet && enemyComponenet.speed <= maxSpeed)
+            enemyComponenet.speed = Mathf.Max(enemyComponenet.speed + speedIncrease, maxSpeed);
     }
 }
